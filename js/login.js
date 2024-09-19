7cae7fbb2c15d5ecd1d603c70d36ac9b29069043
Didar-Druboy <didar.druboy@gmail.com>
2024-09-19 13:09:46 +0600

    log in complete

---
+++ js/login.js
@@ -0,0 +1,14 @@
+// const mobileInput = document.getElementById('mobile-input').value;
+// const pinInput =  document.getElementById('pin-input');
+document.getElementById('btn-login').addEventListener('click', function(event){
+    event.preventDefault();
+    const mobileInputValue = document.getElementById('mobile-input').value;
+    const pinInputValue =  document.getElementById('pin-input').value;
+    
+    if((mobileInputValue === '01827809600' || mobileInputValue === '01866768242')  && pinInputValue === '1234'){
+        window.location.href = "./home.html";
+        // console.log('valid pin and number')
+    }else{
+        window.alert('Invalid Pin or Number')
+    }
+});
