642d6ff47f79ce8f0f621dde8ef25c3e8954dcaf
Didar-Druboy <didar.druboy@gmail.com>
2024-09-21 12:17:07 +0600

    transaction added

--- js/home-new.js
+++ js/home-new.js
@@ -8,10 +8,15 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     if(inputPin === '8242'){
         const newAvailableBalance = availableBalance + inputAmount;
         document.getElementById('available-balance').innerText = newAvailableBalance;
+
+        const p = document.createElement('p');
+        p.innerText = 'Added money: ' + inputAmount + 'Tk.  New Balance: ' + newAvailableBalance + 'Tk.';
+        p.classList.add('bg-green-300', 'p-2');
+        const transactionLogContainer = document.getElementById('transaction-log-container');
+        transactionLogContainer.appendChild(p);
     }else{
         alert('Invalid Pin');
     }
-    // console.log(availableBalanceNumber, inputPin);
 });
 
 // function of cash out money
@@ -22,7 +27,25 @@ document.getElementById('btn-cash-out').addEventListener('click', function(event
     if(inputPin === '8242'){
         const newAvailableBalance = availableBalance - cashOutAmount;
         document.getElementById('available-balance').innerText = newAvailableBalance;
+
+        const p = document.createElement('p');
+        p.innerText = 'Cash Out: ' + cashOutAmount + 'Tk.  New Balance: ' +  newAvailableBalance + 'Tk.';
+        p.classList.add('bg-yellow-300', 'p-2');
+        document.getElementById('transaction-log-container').appendChild(p);
     }else{
         alert('Invalid Pin');
     }
 });
+
+// function to toogle button
+document.getElementById('btn-main-add-money').addEventListener('click', function(){
+    showSectionById('add-money-container');
+})
+
+document.getElementById('btn-main-cash-out').addEventListener('click', function(){
+    showSectionById('cash-out-container');
+})
+
+document.getElementById('btn-main-transaction').addEventListener('click', function(){
+    showSectionById('transaction-container');
+})
--- js/utilities.js
+++ js/utilities.js
@@ -1,8 +1,15 @@
 function getInputValueById(id){
     const inputValue = document.getElementById(id).value;
     return inputValue;
-};
+}
 function getInnerTextById(id){
     const innerText = document.getElementById(id).innerText;
     return innerText;
-};
+}
+function showSectionById(id){
+    document.getElementById('add-money-container').classList.add('hidden');
+    document.getElementById('cash-out-container').classList.add('hidden');
+    document.getElementById('transaction-container').classList.add('hidden');
+
+    document.getElementById(id).classList.remove('hidden');
+}
